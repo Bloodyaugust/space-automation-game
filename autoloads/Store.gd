fa486881adf446a9793be3446161b1016e9fe8eb
Bloodyaugust <greysonrichey@gmail.com>
2022-04-25 16:28:39 -0600

    Initial commit

---
+++ autoloads/Store.gd
@@ -0,0 +1,28 @@
+extends Node
+
+signal state_changed(state_key, substate)
+
+var persistent_store:PersistentStore
+var state: Dictionary = {}
+
+func save_persistent_store() -> void:
+  if ResourceSaver.save(ClientConstants.CLIENT_PERSISTENT_STORE_PATH, persistent_store) != OK:
+    print("Failed to save persistent store")
+
+func set_state(state_key: String, new_state) -> void:
+  state[state_key] = new_state
+  emit_signal("state_changed", state_key, state[state_key])
+  print("State changed: ", state_key, " -> ", state[state_key])
+  
+func _initialize():
+  pass
+
+func _ready():
+  if Directory.new().file_exists(ClientConstants.CLIENT_PERSISTENT_STORE_PATH):
+    persistent_store = load(ClientConstants.CLIENT_PERSISTENT_STORE_PATH)
+
+  if !persistent_store:
+    persistent_store = PersistentStore.new()
+    save_persistent_store()
+
+  call_deferred("_initialize")
---
+++ lib/util/GDUtil.gd
@@ -0,0 +1,28 @@
+extends Node
+
+static func centroid(points:Array) -> Vector2:
+  var centroid:Vector2 = Vector2.ZERO
+
+  if points.size() == 0:
+    return centroid
+
+  for _point in points:
+    centroid += _point
+
+  centroid = centroid / float(points.size())
+
+  return centroid
+
+static func free_children(node):
+  for n in node.get_children():
+      n.free()
+
+static func queue_free_children(node):
+  for n in node.get_children():
+      n.queue_free()
+
+static func reference_safe(node:Node) -> bool:
+  return node != null && !node.is_queued_for_deletion() && is_instance_valid(node)
+
+static func tilemap_global_cell_position(tilemap: TileMap, position: Vector2) -> Vector2:
+  return tilemap.to_global(tilemap.map_to_world(tilemap.world_to_map(tilemap.to_local(position))))
